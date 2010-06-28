57ff96bc59d87b4802e200aa7be22400be56ab40
Devin Cofer <ranguvar13@gmail.com>
2010-06-28 17:27:48 -0400

    Simple hello world with Ant and licensing

---
+++ src/csman/CSMan.java
@@ -0,0 +1,33 @@
+/* 
+ * Pac-Man clone written in Java.
+ * 
+ * Copyright (c) 2010 Devin Cofer <ranguvar@archlinux.us>
+ * 
+ * This file is part of CS-Man.
+ * 
+ * CS-Man is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * CS-Man is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with CS-Man; see the file COPYING.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+package csman;
+
+public class CSMan
+{
+	public static void main(String[] args)
+	{
+		System.out.println("hello, world");
+	}
+
+}
